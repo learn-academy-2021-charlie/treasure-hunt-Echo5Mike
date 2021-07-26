1eba555e91b1e302f8c43d3cab5eb46357a6dc27
Echo5Mike <1Raul.Marquez@gmail.com>
2021-07-26 16:47:15 -0700

    working on it

--- src/App.js
+++ src/App.js
@@ -6,9 +6,25 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
+      board: ["🏔", "🏔", "🏔", "🏔", "🏔", "🏔", "🏔", "🏔", "🏔"],
+      treasureLocation: null
     }
   }
+
+  componentDidMount(){
+    let treasure = Math.floor(Math.random() = this.state.board.length)
+    this.setState({treasureLocation: treasure})
+  }
+
+
+
+  handleGamePlay = (index) =>{
+    const {board} = this.state
+    board[index] = "🌳"
+    this.setState({board: board})
+  }
+
+
 
   render(){
     return(
@@ -16,7 +32,14 @@ class App extends Component{
         <h1>Treasure Hunt Game</h1>
         <div id="gameBoard">
         {this.state.board.map((value, index) => {
-          return <Square value={value}/>
+          return (
+            <Square 
+              value={value} 
+              key={index} 
+              index={index} 
+              handleGamePlay = {this.handleGamePlay}
+            />
+          )
         })}
         </div>
       </>
